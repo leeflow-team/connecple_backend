e8e2a97291097636b8f9f815c3c01b185769a5b2
taeke2 <taeke2@naver.com>
2025-05-04 01:49:00 +0900

    first commit - project init

---
+++ src/main/java/com/connecple/connecple_backend/ConnecpleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.connecple.connecple_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnecpleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnecpleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connecple/connecple_backend/ConnecpleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connecple.connecple_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnecpleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
