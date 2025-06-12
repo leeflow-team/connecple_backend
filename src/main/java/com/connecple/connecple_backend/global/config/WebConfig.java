40be3507c976104e146e24a96fd7306db3797a4d
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-06-12 11:48:38 +0900

    fix : cors 허용 메서드 추가

--- src/main/java/com/connecple/connecple_backend/global/config/WebConfig.java
+++ src/main/java/com/connecple/connecple_backend/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 API에 대해
                 .allowedOrigins("http://localhost:3000", "https://connecple.com", "https://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
