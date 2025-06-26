0aebc551848ecdb1b0c91ca22c8ed639c862e5a3
Daeyeon Kim <58037317+kim946509@users.noreply.github.com>
2025-06-26 21:25:15 +0900

    feat: cors url 추가

--- src/main/java/com/connecple/connecple_backend/global/config/WebConfig.java
+++ src/main/java/com/connecple/connecple_backend/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 API에 대해
-                .allowedOrigins("http://localhost:3000", "https://connecple.com", "https://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://connecple.com", "https://localhost:3000","https://connecple-frontend.vercel.app","https://www.connecple.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
                 .allowedHeaders("*")
                 .allowCredentials(true);
